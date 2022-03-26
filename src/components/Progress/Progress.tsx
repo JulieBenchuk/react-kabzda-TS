873d6e9ac29a610169435f58096c4274f5d090d3
JulieBenchuk <juliebenchuk@gmail.com>
2022-03-26 17:46:03 +0300

    added dependence of number of stars on the progress

---
+++ src/components/Progress/Progress.tsx
@@ -0,0 +1,77 @@
+import React from "react";
+
+function Progress(props: any) {
+    console.log("progress is comming...");
+    if (props.value === 1) {
+        return (
+            <div>
+                <Done selected={true}/>
+                <Done selected={false}/>
+                <Done selected={false}/>
+                <Done selected={false}/>
+                <Done selected={false}/>
+            </div>
+        );
+    } else if (props.value === 2) {
+        return (
+            <div>
+                <Done selected={true}/>
+                <Done selected={true}/>
+                <Done selected={false}/>
+                <Done selected={false}/>
+                <Done selected={false}/>
+            </div>
+        );
+    } else if (props.value === 3) {
+        return (
+            <div>
+                <Done selected={true}/>
+                <Done selected={true}/>
+                <Done selected={true}/>
+                <Done selected={false}/>
+                <Done selected={false}/>
+            </div>
+        );
+    } else if (props.value === 4) {
+        return (
+            <div>
+                <Done selected={true}/>
+                <Done selected={true}/>
+                <Done selected={true}/>
+                <Done selected={true}/>
+                <Done selected={false}/>
+            </div>
+        );
+    } else if (props.value === 5) {
+        return (
+            <div>
+                <Done selected={false}/>
+                <Done selected={false}/>
+                <Done selected={false}/>
+                <Done selected={false}/>
+                <Done selected={false}/>
+            </div>
+        );
+    } else {
+        return (
+            <div>
+                <Done selected={true}/>
+                <Done selected={true}/>
+                <Done selected={true}/>
+                <Done selected={true}/>
+                <Done selected={true}/>
+            </div>
+        );
+    };
+}
+
+function Done(props: any) {
+    console.log("Is's done!");
+    if (props.selected === true) {
+        return <span> <b>done! </b> </span>;
+    } else {
+        return <span>done! </span>;
+    };
+}
+
+export default Progress;
