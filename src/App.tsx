f6c0fbfbbe1984e549604d6de2c27d578a5d567f
JulieBenchuk <juliebenchuk@gmail.com>
2022-05-14 12:39:28 +0300

    add controlled Select

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import React, {useState} from 'react';
+import React, {ChangeEvent, useState} from 'react';
 import './App.css';
 import Accordion from "./components/Accordion/Accordion";
 import Progress from "./components/Progress/Progress";
 import OnOff from "./components/OnOff/OnOff";
+import {Select} from "./components/Select/Select";
 
 const App = () => {
     const [collapsed, setCollapsed] = useState(true);
-    let [switchedOn, setSwitchedOn] = useState(true);
+    const [switchedOn, setSwitchedOn] = useState(true);
+    const [selectValue, setSelectValue] = useState<undefined|string>(undefined)
+    const onChangeSelect = (e: ChangeEvent<HTMLSelectElement>) => {
+        setSelectValue(e.currentTarget.value)
+    }
+
     return (
         <div className={"App"}>
             <PageTitle title={"This is App component!"}/>
@@ -14,6 +20,7 @@ const App = () => {
             <Accordion titleValue={"Menu"} onChange={() => {setCollapsed(!collapsed)}} collapsed={collapsed}/>
             <Accordion titleValue={"Members"} onChange={() => {setCollapsed(!collapsed)}} collapsed={collapsed}/>
             <OnOff onChange = {()=> {setSwitchedOn(!switchedOn)}} switchedOn={switchedOn}/>
+            <Select onChange={onChangeSelect} value={selectValue}/>
         </div>
     );
 }
---
+++ src/components/Select/Select.tsx
@@ -0,0 +1,15 @@
+import React, {ChangeEvent} from "react";
+
+type selectPropsType = {
+    onChange: (e: ChangeEvent<HTMLSelectElement>)=> void
+    value: string | undefined
+}
+export const Select = (props: selectPropsType) => {
+    return <select onChange={props.onChange} value={props.value}>
+        <option value="1">none</option>
+        <option value="2">Minsk</option>
+        <option value="3">Kiev</option>
+        <option value="4">Warsaw</option>
+        <option value="5">Vilnius</option>
+    </select>
+}
