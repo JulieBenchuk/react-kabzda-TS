1bdafa332383b57a439ecf66673c0cb43da21adc
JulieBenchuk <juliebenchuk@gmail.com>
2022-05-15 21:18:01 +0300

    add test to reducer (2/3 passed)

--- src/reducer.test.tsx
+++ src/reducer.test.tsx
@@ -1,4 +1,6 @@
-import {SET_ACTIVE_FALSE, setActiveReducer} from "./reducer";
+import {SET_ACTIVE_FALSE, SET_HOVERED_USER_NEXT, setActiveReducer, setHoveredUserReducer} from "./reducer";
+import {users2, usersPropsType} from "./App";
+import {useReducer} from "react";
 
 
 test("reducer should change active status to false", ()=> {
@@ -6,4 +8,21 @@ test("reducer should change active status to false", ()=> {
     // action
     // expectation
     expect(setActiveReducer(true,{type: SET_ACTIVE_FALSE})).toBe(false)
+});
+
+test("reducer with nonexistent type should throw error", ()=> {
+    //data
+    // action
+    // expectation
+    expect(()=>{setActiveReducer(true,{type: "NONEXISTENT_TYPE"})}).toThrowError();
+})
+
+test("reducer should hover next element after press ArrowDown", ()=>{
+    //data
+    const [hoveredUser, dispatchHovered] = useReducer(setHoveredUserReducer, undefined)
+    // action
+    // expectation
+    expect(()=>{
+        dispatchHovered({type: SET_HOVERED_USER_NEXT, ID: users2[1].id})
+    }).toBe(2);
 })
